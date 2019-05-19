cff843935dcf1e022be688ec6266d60a872852a7
Peter Tyldesley <ptylde@gmail.com>
2019-05-19 10:56:16 +0100

    Add interactivity to ThemeButton

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,45 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useSpring, animated } from 'react-spring'
 
 const Wrapper = styled.div`
-  width: 4rem;
-  height: 4rem;
+  width: 100%;
+  height: 100%;
   background: ${props => props.background};
   color: #333;
-  position: fixed;
   display: flex;
   align-items: center;
   justify-content: center;
-  top: 0;
-  right: 0;
-  margin: 1rem;
   font-size: 2rem;
   -webkit-box-shadow: 3px 4px 5px -2px rgba(0,0,0,0.33);
   -moz-box-shadow: 3px 4px 5px -2px rgba(0,0,0,0.33);
   box-shadow: 3px 4px 5px -2px rgba(0,0,0,0.33);
   border-radius: 4px 20% 4px 20%;
+  cursor: pointer;
 `;
 
-function LightButton() {
+function LightButton({ onClick }) {
   return (
-    <Wrapper>
+    <Wrapper onClick={onClick}>
       <FontAwesomeIcon icon="sun" />
     </Wrapper>
   );
 }
 
-function DarkButton() {
+function DarkButton({ onClick }) {
   return (
-    <Wrapper background="#202B31">
+    <Wrapper onClick={onClick} background="#202B31">
       <FontAwesomeIcon color="white" icon="moon" />
     </Wrapper>
   );
 }
 
 function ThemeButton() {
+  const [hidden, setHidden] = useState(false);
+  const [theme, setTheme] = useState('light');
+
+  const scrollListener = event => setHidden(window.pageYOffset > 300);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollListener);
+    return () => document.removeEventListener('scroll', scrollListener);
+  }, []);
+
+  const props = useSpring({
+    position: 'fixed',
+    width: '4rem',
+    height: '4rem',
+    top: hidden ? '-3.7rem' : '0rem',
+    right: 0,
+    marginTop: '0.5rem',
+    marginRight: '2rem',
+  });
+
+  const renderButton = () => {
+    return theme === 'light'
+      ? <DarkButton onClick={() => {
+        setHidden(false);
+        setTheme('dark');
+      }} />
+      : <LightButton onClick={() => {
+        setHidden(false);
+        setTheme('light');
+      }} />;
+  }
+
   return (
-    <DarkButton />
+    <animated.div onMouseEnter={() => setHidden(false)} style={props}>
+      {renderButton()}
+    </animated.div>
   );
 }
 
