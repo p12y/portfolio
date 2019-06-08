62d52cd07df47e8972b4201c5cc793e9884d3988
Peter Tyldesley <ptylde@gmail.com>
2019-06-08 13:11:19 +0100

    Create work layout

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 import Title from './Title';
 import Container from './styled/Container';
+import media from 'styles/media';
+
+const Grid = styled.div`
+  display: grid;
+  flex-wrap: wrap;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
+  ${media.tablet`
+    grid-template-columns: repeat(auto-fit, minmax(40%, 1fr));
+  `}
+`;
+
+const WorkCard = styled.div`
+  position: relative;
+  width: 80%;
+  height: 18rem;
+  margin: auto;
+  background: white;
+  border-radius: 4px;
+  background: #2BC0E4;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #EAECC6, #2BC0E4);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #EAECC6, #2BC0E4); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
 
 function Work() {
   return (
     <Container>
       <Title text="Work" />
+      <Grid>
+        <WorkCard></WorkCard>
+        <WorkCard></WorkCard>
+        <WorkCard></WorkCard>
+        <WorkCard></WorkCard>
+        <WorkCard></WorkCard>
+        <WorkCard></WorkCard>
+      </Grid>
     </Container>
   );
 };
--- src/components/styled/Container.js
+++ src/components/styled/Container.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import media from 'styles/media';
 
 const Container = styled.div`
-  margin: 0 5rem 0 5rem;
+  margin: 0 5rem 5rem 5rem;
   ${media.phone`
-  margin: 0 1rem 0 1rem;
+  margin: 0 1rem 5rem 1rem;
   `}
 `;
 
