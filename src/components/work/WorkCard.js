cbc9579ac54b828708820ce9afc8698a3f886090
Peter Tyldesley <ptylde@gmail.com>
2019-06-10 19:02:04 +0100

    Add gradients to workItems

--- src/components/work/WorkCard.js
+++ src/components/work/WorkCard.js
@@ -8,9 +8,8 @@ const Card = styled.div`
   margin: auto;
   background: white;
   border-radius: ${({ theme }) => theme.borderRadius};
-  background: #2BC0E4;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #EAECC6, #2BC0E4);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #EAECC6, #2BC0E4); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: #2BC0E4; /* fallback for old browsers */
+  background: ${props => props.background};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -18,12 +17,12 @@ const Card = styled.div`
 
 const Title = styled.h2`
   font-family: ${({ theme }) => theme.fonts.title};
-  color: ${({ theme }) => theme.titleColor};
+  color: white;
 `;
 
 const WorkCard = (props) => {
   return (
-    <Card>
+    <Card {...props}>
       <Title>{props.title}</Title>
     </Card>
   );
--- src/data/workItems.js
+++ src/data/workItems.js
@@ -4,14 +4,18 @@
 export default [
   {
     title: 'Test 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.',
+    background: 'linear-gradient(to right, #6dd5ed, #2193b0)'
+
   },
   {
     title: 'Test 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.',
+    background: 'linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5)',
   },
   {
     title: 'Test 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.',
+    background: 'linear-gradient(to right, #0083B0, #00B4DB)',
   },
 ];
