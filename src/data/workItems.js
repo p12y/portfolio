f01d301810e7d2b43fc7dd21296e69606529a7ca
Peter Tyldesley <ptylde@gmail.com>
2019-09-23 12:35:47 +1000

    Add work item data

--- src/data/workItems.js
+++ src/data/workItems.js
@@ -1,61 +1,48 @@
+const generateImages = appName => {
+  const images = [];
+  for (let i = 1; i <= numImages[appName]; i += 1) {
+    images.push({
+      original: require(`images/screenshots/${appName}/${i}.png`),
+    });
+  }
+  return images;
+};
+
+const numImages = {
+  shrnk: 3,
+  'time-planner': 5,
+  'meetup-scheduler': 4,
+};
+
 /**
  * List of portfolio work items to be displayed
  */
 export default [
   {
-    projectTitle: 'Test 1',
-    projectInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.',
+    projectTitle: 'Time Planner',
+    projectInfo:
+      'Time planning application built with React, Firebase and Material UI. Complete with SSO capability, the app allows employees to create time plans and share them with other members of the team.',
     background: 'linear-gradient(to right, #6dd5ed, #2193b0)',
-    images: [
-      {
-        original: 'https://dummyimage.com/800x480/',
-        thumbnail: 'https://dummyimage.com/250x150/',
-      },
-      {
-        original: 'https://dummyimage.com/800x480/',
-        thumbnail: 'https://dummyimage.com/250x150/',
-      }
-    ],
-    tags: [
-      'React',
-      'Firebase',
-    ],
+    images: generateImages('time-planner'),
+    tags: ['React', 'Firebase', 'SSO', 'Google App Engine'],
   },
   {
-    projectTitle: 'Test 2',
-    projectInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.',
+    projectTitle: 'Meetup Polls',
+    projectInfo:
+      'Polling application built with React and Firebase with realtime functionality. Designed to help friends & family pick a day to meet up that works for everyone.',
     background: 'linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5)',
-    images: [
-      {
-        original: 'https://dummyimage.com/800x480/',
-        thumbnail: 'https://dummyimage.com/250x150/',
-      },
-      {
-        original: 'https://dummyimage.com/800x480/',
-        thumbnail: 'https://dummyimage.com/250x150/',
-      }
-    ],
-    tags: [
-      'React',
-      'MongoDB',
-    ],
+    url: 'https://meetup-scheduler-development.firebaseapp.com/',
+    images: generateImages('meetup-scheduler'),
+    tags: ['React', 'Firebase', 'Cloud Functions'],
   },
   {
-    projectTitle: 'Test 3',
-    projectInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed vulputate.',
+    projectTitle: 'Shrnk',
+    projectInfo:
+      'A URL shortening application and API that takes a long URL and returns an encoded URL, which will redirect back to the original. Built with Vue, Express and MongoDB.',
+    url: 'https://shrnk-url.appspot.com/',
     background: 'linear-gradient(to right, #0083B0, #00B4DB)',
-    images: [
-      {
-        original: 'https://dummyimage.com/800x480/',
-        thumbnail: 'https://dummyimage.com/250x150/',
-      },
-      {
-        original: 'https://dummyimage.com/800x480/',
-        thumbnail: 'https://dummyimage.com/250x150/',
-      }
-    ],
-    tags: [
-      'NodeJS',
-    ],
+    images: generateImages('shrnk'),
+
+    tags: ['NodeJS', 'Express', 'Vue', 'MongoDB', 'Google App Engine'],
   },
 ];
